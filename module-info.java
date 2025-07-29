8de65b7d54e7990285e13ea15b2b0e0e8c62b00e
Nashville khent luna <nkluna69274@liceo.edu.ph>
2025-07-30 01:14:22 +0800

    Add files via upload

---
+++ module-info.java
@@ -0,0 +1,16 @@
+module com.example.luna_scoreexamproject {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.example.luna_scoreexamproject to javafx.fxml;
+    exports com.example.luna_scoreexamproject;
+}
