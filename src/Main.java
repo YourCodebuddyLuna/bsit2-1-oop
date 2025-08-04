4e8909d4e0c6fd3c1140a15da3e382314dbc4cb0
Nashville khent luna <nkluna69274@liceo.edu.ph>
2025-08-04 22:03:14 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,61 @@
-import java.util.Scanner;
+public class Book {
+    String title;
+    String author;
+    int pages;
+    boolean isAvailable;
 
-public class Main {
-    static int number = 0;
-    public static void main(String[] args) {
+ 
+    public Book(String title, String author, int pages) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.isAvailable = true;
+        System.out.println("A new book '" + title + "' by " + author + " has been added to the library!");
+    }
 
-        Scanner scanner = new Scanner(System.in);
-        for (int i = 1; i <= 5; i++) {
-            System.out.print("Enter number " + i + ": ");
-            number = number + scanner.nextInt();
+    
+    public void displayInfo() {
+        System.out.println("Title: " + title);
+        System.out.println("Author: " + author);
+        System.out.println("Pages: " + pages);
+        System.out.println("Available: " + (isAvailable ? "Yes" : "No"));
+        System.out.println("-------------------------");
+    }
 
+    
+    public void borrowBook() {
+        if (isAvailable) {
+            isAvailable = false;
+            System.out.println("'" + title + "' has been borrowed.");
+        } else {
+            System.out.println("'" + title + "' is currently not available.");
         }
-        System.out.println("Total Number: " + number);
+    }
+
+    
+    public void returnBook() {
+        if (!isAvailable) {
+            isAvailable = true;
+            System.out.println("'" + title + "' has been returned.");
+        } else {
+            System.out.println("'" + title + "' was not borrowed.");
+        }
+    }
+
+    
+    public static void main(String[] args) {
+        Book book1 = new Book("The Great Gatsby", "F. Scott Fitzgerald", 180);
+        Book book2 = new Book("1984", "George Orwell", 328);
+        Book book3 = new Book("To Kill a Mockingbird", "Harper Lee", 281);
+
+        book1.displayInfo();
+        book2.displayInfo();
+        book3.displayInfo();
+
+        book1.borrowBook();
+        book1.displayInfo();
+
+        book1.returnBook();
+        book1.displayInfo();
     }
 }
