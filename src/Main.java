c8e8e81f6447dba34122a51a7524f381797f159f
Nashville khent luna <nkluna69274@liceo.edu.ph>
2025-08-07 10:45:17 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,49 @@
-import java.util.Scanner;
 
-public class Main {
-    static int number = 0;
-    public static void main(String[] args) {
+public class Students {
+    String name;
+    String course;
+    int age;
+    double Grade1;
+    double Grade2;
+    double Grade3;
+    double Grade;
 
-        Scanner scanner = new Scanner(System.in);
-        for (int i = 1; i <= 5; i++) {
-            System.out.print("Enter number " + i + ": ");
-            number = number + scanner.nextInt();
 
-        }
-        System.out.println("Total Number: " + number);
+
+    public Students(String name, String course, int age ) {
+
+        this.name = name;
+        this.course = course;
+        this.age = age;
+
+        System.out.println("New Student '" + name +"'course" + age +"has been added to your Students list");
     }
-}
+
+    public void displayInfo () {
+        System.out.println("Name: " + name);
+        System.out.println("course: " +  course);
+        System.out.println("age:" + age);
+
+    }
+
+
+    public void calculateGrades () {
+
+        if ( Grade1 >= 90) {
+            System.out.println(" the output will be A");
+        }
+
+        if (Grade2 >= 80-89) {
+            System.out.println("the output will be B");
+        }
+
+        if(Grade3 <= 70-79) {
+            System.out.println("the output will be C");
+        }
+
+        if ( Grade <= 60-69) {
+            System.out.println("the output will be D");
+        }else {
+            System.out.println(" the letter grade: F Failed");
+        }
+    }
